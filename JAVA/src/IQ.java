bb746c7e0e8b780eae64bfd1332ef52ba15f5719
kevini58333 <kirvanipour.58333@farnborough.ac.uk>
2015-11-03 11:47:38 +0000

    commit 3rd November

---
+++ JAVA/src/IQ.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class IQ {
+	public static void main(String [] args){
+		int numIQ;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("What is your IQ?");
+		numIQ=sc.nextInt();
+		
+		
+		if(numIQ < 50 && numIQ >200 ) {
+			
+		}
+		System.out.println("your IQ is: " + testIQ + testIQ2);
+	}
+
+}
